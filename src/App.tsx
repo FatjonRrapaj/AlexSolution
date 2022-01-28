baa1632ff620a91a8165ba2b59ad05d8a501eece
Fatjon Rrapaj <38084457+FatjonRrapaj@users.noreply.github.com>
2022-01-28 11:35:14 +0100

    remove comment

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import ErrorBoundary from './ErrorBoundary';
 export default function App() {
   return (
     <div className="flex min-h-screen flex-col ml-1 mr-1 mb-10 mt-10">
-      {/* <span>Test</span> */}
       <div className="customText text-5xl text-basic-blue ml-20 mb-10">Population der Staaten</div>
       <Suspense fallback={<h1 className="ml-40">Loading ...</h1>}>
         <ErrorBoundary>
