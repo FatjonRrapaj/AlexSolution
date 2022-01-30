d0162aee5d99f77e312888d87a748f55c1c54d81
Fatjon Rrapaj <38084457+FatjonRrapaj@users.noreply.github.com>
2022-01-30 07:23:41 +0100

    rename population & jahr

--- src/State.tsx
+++ src/State.tsx
@@ -8,11 +8,11 @@ const State: React.FC<Props> = ({ stateName, year, population }: Props) => (
   <div className="bg-white shadow-md p-5 rounded-lg flex-col ml-10 mb-10">
     <div className="customText">{stateName}</div>
     <div className="separator" />
-    <div className="customText text-basic-grey">Population: {year}</div>
-    <div className="separator" />
     <div className="customText text-basic-grey">
-      Jahr: {Math.round(Math.abs(Number(population)) / 1.0e6) / 10 + 'M'}
+      Population: {Math.round(Math.abs(Number(population)) / 1.0e6) / 10 + 'M'}
     </div>
+    <div className="separator" />
+    <div className="customText text-basic-grey">Jahr: {year}</div>
   </div>
 );
 
